3c7e03cc5aadc213c15fd5c215265639d6d7c982
Millowise01 <l.carew@alustudent.com>
2024-09-14 10:21:02 +0000

    phone number and credit card number

--- regex_data_extraction.py
+++ regex_data_extraction.py
@@ -13,12 +13,18 @@ def extract_urls(text):
     return re.findall(url_pattern, text)
 
 # 3. Extracting Phone Numbers (various formats)
+def extract_phone_numbers(text):
+    phone_pattern = r'(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}'
+    return re.findall(phone_pattern, text)
 
 # 4. Extracting Time in 12-hour or 24-hiour format
 
 # 5. Extracting Hashtags
 
 # 6. Extracting Credit Card Numbers
+def extract_credit_cards(text):
+    credit_card_pattern = r'(\d{4}[-\s]?){3}\d{4}'
+    return re.findall(credit_card_pattern, text)
 
 # 7. Extracting HTML Tags
 def extract_html_tags(text):
@@ -38,21 +44,21 @@ def main():
     print("Extracted information")
     print("------------------------------------------------------------")
     print()
-    print("Extracted Email Addresses:", extract_emails(sample_text))
+    #print("Extracted Email Addresses:", extract_emails(sample_text))
     print()
     print("Extracted URLs:", extract_urls(sample_text))
     print()
     print("Extracted Phone Numbers:", extract_phone_numbers(sample_text))
     print()
-    print("Extracted Times:", extract_times(sample_text))
+    #print("Extracted Times:", extract_times(sample_text))
     print()
-    print("Extracted Hashtags:", extract_hashtags(sample_text))
+    #print("Extracted Hashtags:", extract_hashtags(sample_text))
     print()
     print("Extracted Credit Card Numbers:", extract_credit_cards(sample_text))
     print()
     print("Extracted HTML Tags:", extract_html_tags(sample_text))
     print()
-    print("Extracted Currency Amounts:", extract_currency_amounts(sample_text))
+    #print("Extracted Currency Amounts:", extract_currency_amounts(sample_text))
     print()
 
 if __name__ == '__main__':
